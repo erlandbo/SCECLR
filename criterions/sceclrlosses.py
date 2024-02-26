e309d7289188bfd52105897f0d1b3e8c45c2973a
erlandbo <erlandolssonsm@gmail.com>
2024-02-26 23:09:45 +0100

    Added sceclr loss

---
+++ criterions/sceclrlosses.py
@@ -0,0 +1,150 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class SCECLRLoss(nn.Module):
+    def __init__(self, N=60_000, rho=-1, alpha=0.5, S_init=2.0):
+        super().__init__()
+        # buffer's current values can be loaded using the state_dict of the module which might be useful to know
+        self.register_buffer("xi", torch.zeros(1, ))  # weighted sum q
+        self.register_buffer("omega", torch.zeros(1, ))  # count q
+        self.register_buffer("N", torch.zeros(1, ) + N)  # N samples in dataset
+        self.register_buffer("s_inv", torch.zeros(1, ) + N**S_init)
+        self.register_buffer("alpha", torch.zeros(1, ) + alpha)
+        self.register_buffer("rho", torch.zeros(1, ) + rho)  # Automatically set rho or constant
+
+    @torch.no_grad()
+    def update_s(self, qii, qij):
+        self.xi = torch.zeros(1, ).to(qii.device)
+        self.omega = torch.zeros(1, ).to(qij.device)
+
+        # Attraction
+        Bii = qii.shape[0]
+        self.xi = self.xi + torch.sum(self.alpha * qii.detach())
+        self.omega = self.omega + self.alpha * Bii
+
+        # Repulsion
+        Bij = qij.shape[0]
+        self.xi = self.xi + torch.sum((1 - self.alpha) * qij.detach())
+        self.omega = self.omega + (1 - self.alpha) * Bij
+
+        # Automatically set rho or constant
+        momentum = self.N.pow(2) / (self.N.pow(2) + self.omega) if self.rho > 0 else self.rho
+        weighted_sum_count = self.xi / self.omega
+        self.s_inv = momentum * self.s_inv + (1 - momentum) * self.N.pow(2) * weighted_sum_count
+
+
+class StudtSCECLRLoss(SCECLRLoss):
+    def __init__(self, N=60_000, rho=-1, alpha=0.5, S_init=2.0, dof=1):
+        super(StudtSCECLRLoss, self).__init__(N=60_000, rho=-1, alpha=0.5, S_init=2.0)
+
+    def forward(self, z):
+
+        B = z.shape[0] // 2
+        zi, zj = z[0:B], z[B:]
+        # TODO add dof
+
+        q = 1.0 / ( torch.cdist(zi, zj, p=2).pow(2) + 1.0 )  # (B,E),(B,E) -> (B,B)
+
+        # Attraction
+        qii = torch.diag(q).unsqueeze(1)  # (B,1)
+        attractive_forces = - torch.log(qii)
+
+        # Repulsion
+        qij = q[~torch.eye(B, dtype=torch.bool)]  # off diagonal
+        Z = torch.sum(q.detach(), dim=1, keepdim=True).requires_grad_(False)  # (B,B) -> (B,1)
+        s_hat = self.N.pow(2) / self.s_inv
+        repulsive_forces = torch.sum(q / Z, dim=1, keepdim=True) * s_hat
+
+        loss = attractive_forces.mean() + repulsive_forces.mean()
+        self.update_s(qii, qij)
+        return loss
+
+
+class GaussianSCECLRLoss(SCECLRLoss):
+    def __init__(self, N=60_000, rho=-1, alpha=0.5, S_init=2.0):
+        super(GaussianSCECLRLoss, self).__init__(N=60_000, rho=-1, alpha=0.5, S_init=2.0)
+
+    def forward(self, z):
+        B = z.shape[0] // 2
+        zi, zj = z[0:B], z[B:]
+        # TODO add var
+        allpairnegdist = - torch.cdist(zi, zj, p=2).pow(2)  # (B,E),(B,E) -> (B,B)
+
+        q = torch.exp(allpairnegdist)
+        # Attraction
+        allpairnegdist_ii = torch.diag(allpairnegdist).unsqueeze(1)  # (B,1)
+        attractive_forces = - allpairnegdist_ii   # log cancels exp
+        qii = torch.diag(q).unsqueeze(1)
+        # Repulsion
+        qij = q[~torch.eye(B, dtype=torch.bool)]  # off diagonal
+        Z = torch.sum(q.detach(), dim=1, keepdim=True).requires_grad_(False)  # (B,B) -> (B,1)
+        s_hat = self.N.pow(2) / self.s_inv
+        repulsive_forces = torch.sum(q / Z, dim=1, keepdim=True) * s_hat
+
+        loss = attractive_forces.mean() + repulsive_forces.mean()
+        self.update_s(qii, qij)
+        return loss
+
+
+class CosineSCECLRLoss(SCECLRLoss):
+    def __init__(self, N=60_000, rho=-1, alpha=0.5, S_init=2.0, tau=0.1):
+        super(CosineSCECLRLoss, self).__init__(N=60_000, rho=-1, alpha=0.5, S_init=2.0)
+        self.tau = tau
+
+    def forward(self, z):
+        B = z.shape[0] // 2
+        z = F.normalize(z, dim=1)
+        zi, zj = z[0:B], z[B:2*B]
+
+        # TODO add var
+        cossim = torch.matmul(zi, zj.T) / self.tau  # (B,E) @ (E,B) -> (N,B)
+
+        q = torch.exp(cossim)
+
+        # Attraction
+        cossim_ii = torch.diag(cossim).unsqueeze(1)  # (B,1)
+        attractive_forces = cossim_ii   # log cancels exp
+        qii = torch.diag(q).unsqueeze(1)
+
+        # Repulsion
+        qij = q[~torch.eye(B, dtype=torch.bool)]  # off diagonal
+        Z = torch.sum(q.detach(), dim=1, keepdim=True).requires_grad_(False)  # (B,B) -> (B,1)
+        s_hat = self.N.pow(2) / self.s_inv
+        repulsive_forces = torch.sum(q / Z, dim=1, keepdim=True) * s_hat
+
+        loss = attractive_forces.mean() + repulsive_forces.mean()
+        self.update_s(qii, qij)
+        return loss
+
+
+class DotProdSCECLRLoss(SCECLRLoss):
+    def __init__(self, N=60_000, rho=-1, alpha=0.5, S_init=2.0, l2_reg=0.01):
+        super(DotProdSCECLRLoss, self).__init__(N=60_000, rho=-1, alpha=0.5, S_init=2.0)
+        self.l2_reg = l2_reg
+
+    def forward(self, z):
+        B = z.shape[0] // 2
+        zi, zj = z[0:B], z[B:2*B]
+
+        # TODO add var
+        dotsim = torch.matmul(zi, zj.T)  # (B,E) @ (E,B) -> (N,B)
+
+        q = torch.exp(dotsim)
+
+        # Attraction
+        dotsim_ii = torch.diag(dotsim).unsqueeze(1)  # (B,1)
+        attractive_forces = dotsim_ii   # log cancels exp
+        qii = torch.diag(q).unsqueeze(1)
+
+        # Repulsion
+        qij = q[~torch.eye(B, dtype=torch.bool)]  # off diagonal
+        Z = torch.sum(q.detach(), dim=1, keepdim=True).requires_grad_(False)  # (B,B) -> (B,1)
+        s_hat = self.N.pow(2) / self.s_inv
+        repulsive_forces = torch.sum(q / Z, dim=1, keepdim=True) * s_hat
+
+        loss = attractive_forces.mean() + repulsive_forces.mean() + self.l2_reg * z.sum()
+        self.update_s(qii, qij)
+        return loss
+
---
+++ criterions/scelosses.py
@@ -0,0 +1,153 @@
+import torch
+from torch import nn
+from torch.nn import functional as F
+
+
+class SCELoss(nn.Module):
+    def __init__(self, N=60_000, rho=-1, alpha=0.5, S_init=2.0):
+        super().__init__()
+        # buffer's current values can be loaded using the state_dict of the module which might be useful to know
+        self.register_buffer("xi", torch.zeros(1, ))  # weighted sum q
+        self.register_buffer("omega", torch.zeros(1, ))  # count q
+        self.register_buffer("N", torch.zeros(1, ) + N)  # N samples in dataset
+        self.register_buffer("s_inv", torch.zeros(1, ) + N**S_init)
+        self.register_buffer("alpha", torch.zeros(1, ) + alpha)
+        self.register_buffer("rho", torch.zeros(1, ) + rho)  # Automatically set rho or constant
+
+    @torch.no_grad()
+    def update_s(self, qii, qij):
+        # Attraction
+        Bii = qii.shape[0]
+        self.xi = self.xi + torch.sum(self.alpha * qii.detach())
+        self.omega = self.omega + self.alpha * Bii
+        # Repulsion
+        Bij = qij.shape[0]
+        self.xi = self.xi + torch.sum((1 - self.alpha) * qij.detach())
+        self.omega = self.omega + (1 - self.alpha) * Bij
+        # Automatically set rho or constant
+        momentum = self.N.pow(2) / (self.N.pow(2) + self.omega) if self.rho > 0 else self.rho
+        weighted_sum_count = self.xi / self.omega
+        self.s_inv = momentum * self.s_inv + (1 - momentum) * self.N.pow(2) * weighted_sum_count
+
+
+class StudtSCELoss(SCELoss):
+    def __init__(self, N=60_000, rho=-1, alpha=0.5, S_init=2.0, dof=1):
+        super(StudtSCELoss, self).__init__(N=60_000, rho=-1, alpha=0.5, S_init=2.0)
+
+    def forward(self, z):
+        self.xi = torch.zeros(1, ).to(z.device)
+        self.omega = torch.zeros(1, ).to(z.device)
+
+        B = z.shape[0] // 2
+        zi, zj = z[0:B], z[B:]
+        # TODO add dof
+        # Attraction
+        pairdist_ii = F.pairwise_distance(zi, zj, keepdim=True)
+        qii = 1.0 / ( pairdist_ii.pow(2) + 1.0 )  # (B,1)
+        attractive_forces = torch.mean(- torch.log(qii))
+
+        # Repulsion
+        pairdist_ij = F.pairwise_distance(zi, torch.roll(zj, shifts=-1, dims=0), keepdim=True)
+        qij = 1.0 / ( pairdist_ij.pow(2) + 1.0 )  # (B,1)
+
+        s_hat = self.N.pow(2) / self.s_inv
+        repulsive_forces = torch.mean(qij * s_hat)
+
+        loss = attractive_forces + repulsive_forces
+        self.update_s(qii, qij)
+        return loss
+
+
+class GaussianSCELoss(SCELoss):
+    def __init__(self, N=60_000, rho=-1, alpha=0.5, S_init=2.0, var=1):
+        super(GaussianSCELoss, self).__init__(N=60_000, rho=-1, alpha=0.5, S_init=2.0)
+
+    def forward(self, z):
+        self.xi = torch.zeros(1, ).to(z.device)
+        self.omega = torch.zeros(1, ).to(z.device)
+
+        B = z.shape[0] // 2
+        zi, zj = z[0:B], z[B:]
+        # TODO add var
+        # Attraction
+        pairdist_ii = F.pairwise_distance(zi, zj, keepdim=True)
+        qii = torch.exp(-pairdist_ii.pow(2))  # (B,1)
+        qii = qii.clamp(torch.finfo(float).eps)
+
+        attractive_forces = torch.mean(- pairdist_ii)  # log cancels exp
+
+        # Repulsion
+        pairdist_ij = F.pairwise_distance(zi, torch.roll(zj, shifts=-1, dims=0), keepdim=True)
+        qij = torch.exp(-pairdist_ij.pow(2))
+        qij = qij.clamp(torch.finfo(float).eps)
+
+        s_hat = self.N.pow(2) / self.s_inv
+        repulsive_forces = torch.mean(qij * s_hat)
+
+        loss = attractive_forces + repulsive_forces
+        self.update_s(qii, qij)
+        return loss
+
+
+# class CosineSCELoss(SCELoss):
+#     def __init__(self, N=60_000, rho=-1, alpha=0.5, S_init=2.0, tau=0.1):
+#         super(SCELoss, self).__init__(N=60_000, rho=-1, alpha=0.5, S_init=2.0)
+#         self.register_buffer("tau", torch.zeros(1, ) + tau)
+#
+#     def forward(self, z):
+#         self.xi = torch.zeros(1, ).to(z.device)
+#         self.omega = torch.zeros(1, ).to(z.device)
+#
+#         B = z.shape[0] // 2
+#         zi, zj = z[0:B], z[B:]
+#         # Attraction
+#         cossim_ii = F.cosine_similarity(zi, zj, dim=1).unsqueeze(1)  # (B,1)
+#         qii = torch.exp(cossim_ii)
+#
+#         attractive_forces = torch.mean(cossim_ii / self.tau)  # log cancels exp
+#
+#         # Repulsion
+#         cossim_ij = F.cosine_similarity(zi, torch.roll(zj, shifts=-1, dims=0), dim=1).unsqueeze(1)  # (B,1)
+#         qij = torch.exp(cossim_ij / self.tau)
+#
+#         s_hat = self.N.pow(2) / self.s_inv
+#         repulsive_forces = torch.mean(qij * s_hat)
+#
+#         loss = attractive_forces + repulsive_forces
+#         self.update_s(qii, qij)
+#         return loss
+#
+#
+# class DotProdL2SCELoss(SCELoss):
+#     def __init__(self, N=60_000, rho=-1, alpha=0.5, S_init=2.0, tau=0.1):
+#         super(SCELoss, self).__init__(N=60_000, rho=-1, alpha=0.5, S_init=2.0)
+#         self.register_buffer("tau", torch.zeros(1, ) + tau)
+#
+#     def forward(self, z):
+#         self.xi = torch.zeros(1, ).to(z.device)
+#         self.omega = torch.zeros(1, ).to(z.device)
+#
+#         B = z.shape[0] // 2
+#         zi, zj = z[0:B], z[B:]
+#         # Attraction
+#         dotprod_ii = torch.sum(zi * zj, dim=1, keepdim=True)  # (B,1)
+#         qii = torch.exp(dotprod_ii)
+#
+#         attractive_forces = torch.mean(dotprod_ii)  # log cancels exp
+#
+#         # Repulsion
+#         dotprod_ii = torch.sum(zi * torch.roll(zj, shifts=-1, dims=0), dim=1, keepdim=True)  # (B,1)
+#         qii = torch.exp(dotprod_ii)
+#
+#         attractive_forces = torch.mean(dotprod_ii)  # log cancels exp
+#
+#         cossim_ij = F.cosine_similarity(zi, torch.roll(zj, shifts=-1, dims=0), dim=1).unsqueeze(1)  # (B,1)
+#         qij = torch.exp(cossim_ij / self.tau)
+#
+#         s_hat = self.N.pow(2) / self.s_inv
+#         repulsive_forces = torch.mean(qij * s_hat)
+#
+#         loss = attractive_forces + repulsive_forces
+#         self.update_s(qii, qij)
+#         return loss
+
