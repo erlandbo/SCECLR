fe820fbf3969fe977a5b0ed95222406b9a583693
erlandbo <erlandolssonsm@gmail.com>
2024-03-16 20:15:36 +0100

    Added sceclrv4, removed BN in projector and added normal-init for 2D linear for stage 2.

--- run_experiments_sceclrv1.sh
+++ run_experiments_sceclrv1.sh
@@ -1,1 +1,2 @@
-python main.py --outfeatures 256 --mlp_hidden_features 2048 --eval_epoch 5 --batchsize 1024 --rho 0.99999 --epochs 1000 50 250 --criterion sceclrv1 --alpha 0.25 --numworkers 20 --metric cosine --s_init 2.0 --lr_anneal cosine_anneal --weight_decay 5.0e-4
+python main.py --outfeatures 128 --mlp_hidden_features 1024 --eval_epoch 10 --batchsize 512 --rho 0.99999 --epochs 501 50 250 --criterion sceclrv1 --alpha 0.25 --numworkers 10 --metric cauchy --s_init 2.0 --lr_anneal cosine_anneal --weight_decay 5.0e-4
+python main.py --outfeatures 128 --mlp_hidden_features 1024 --eval_epoch 10 --batchsize 512 --rho 0.99999 --epochs 501 50 250 --criterion sceclrv1 --alpha 0.1 --numworkers 10 --metric cauchy --s_init 2.0 --lr_anneal cosine_anneal --weight_decay 5.0e-4
